9dffee29c1f1b17017a7df14ba7598b7ebb235b5
Martin Ciro <martinciro11@gmail.com>
2024-11-29 14:57:31 -0500

    Se crea interfaz basica

---
+++ js/script.js
@@ -0,0 +1,93 @@
+import { insertHtml } from './utils.js';
+// document.addEventListener('DOMContentLoaded', () => {
+//     insertHtml('head');
+// });
+
+ // Función para mostrar u ocultar la contraseña
+// document.getElementById("togglePassword").addEventListener("click", function() {
+//     const passwordField = document.getElementById("password");
+//     const passwordType = passwordField.type === "password" ? "text" : "password";
+//     passwordField.type = passwordType;
+// });
+
+// document.querySelectorAll('#cerrarModal').forEach(button => {
+//     button.addEventListener('click', function () {
+//         document.getElementById('modal').classList.add('hidden');
+//     });
+// });
+
+insertHtml('sidebar'); 
+insertHtml('nav-menu');
+insertHtml('footer');
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownButton = document.getElementById('nav-menu-small');
+    const sidebar = document.getElementById('sidebar');
+    const carrusel = document.getElementById('carrusel');
+
+    if (dropdownButton && sidebar) {
+        dropdownButton.addEventListener('click', function (event) {
+            event.stopPropagation();
+            sidebar.classList.toggle('hidden');
+            sidebar.classList.toggle('active');
+        });
+
+        document.addEventListener('click', function (event) {
+            if (!dropdownButton.contains(event.target) && !sidebar.contains(event.target) && !sidebar.classList.contains('hidden')) {
+                sidebar.classList.add('hidden');
+                sidebar.classList.remove('active');
+            }
+        });
+    }
+
+    if (carrusel) {
+        carrusel.innerHTML = `
+            <div class="swiper mySwiper flex items-center rounded">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide"><img src="src/img/carrusel/1.jpg" class="hover:cursor-pointer" alt="Final nacional futbol sala"></div>
+                    <div class="swiper-slide"><img src="src/img/carrusel/2.jpg" alt="Resultado tolima vs caldas"></div>
+                    <div class="swiper-slide"><img src="src/img/carrusel/3.jpg" alt="Resultado caldas vs lfb"></div>
+                    <div class="swiper-slide"><img src="src/img/carrusel/4.jpg" alt="Festival de futbol chinchina"></div>
+                </div>
+                <!-- Botones de navegación -->
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+
+                <!-- Paginación -->
+                <div class="swiper-pagination mt-auto"></div>
+            </div>
+        `;
+
+        new Swiper('.mySwiper', {
+            spaceBetween: 10,
+            centeredSlides: true,
+            autoplay: {
+                delay: 3500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                640: {
+                    slidesPerView: 1,
+                    spaceBetween: 10,
+                },
+                768: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                1024: {
+                    slidesPerView: 4,
+                    spaceBetween: 40,
+                },
+            },
+        });
+    };
+});
---
+++ js/utils.js
@@ -0,0 +1,16 @@
+export function insertHtml(html, nomId = null) {
+    if (nomId === null)  nomId = html
+    
+    fetch(`../components/${html}.html`)
+        .then(response => {
+            if (!response.ok) throw new Error('Network response was not ok ' + response.statusText);
+            return response.text();
+        })
+        .then(data => {
+            const element = document.getElementById(nomId);
+            element ? element.innerHTML = data : null;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
