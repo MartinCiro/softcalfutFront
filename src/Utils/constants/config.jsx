307efd2c5479639d0213f468592ea146ede1568e
martinciro <martinciro11@gmail.com>
2025-08-20 10:22:32 -0400

    Se actualiza config mode

--- src/Utils/constants/config.jsx
+++ src/Utils/constants/config.jsx
@@ -2,13 +2,13 @@ const env = typeof import.meta !== 'undefined' ? import.meta.env : process.env;
 
 export default {
   env: {
-    NODE_ENV: env.MODE || env.NODE_ENV,
-    VITE_API_URL: (env.MODE || env.NODE_ENV) === 'production' 
+    NODE_ENV: env.NODE_ENV,
+    VITE_API_URL: env.NODE_ENV === 'production' 
       ? env.VITE_API_URL_PROD 
       : env.VITE_API_URL_DEV || 'http://localhost:3000',
   },
-  isProduction: (env.MODE || env.NODE_ENV) === 'production',
-  server: (env.MODE || env.NODE_ENV) === 'production'
+  isProduction: env.NODE_ENV === 'production',
+  server: env.NODE_ENV === 'production'
     ? env.VITE_API_URL_PROD 
     : env.VITE_API_URL_DEV || 'http://localhost:3000'
 };
